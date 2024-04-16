03859083a862c3acdcd97ef97c21fb41b816931a
guisadev <giseesaavedra537@gmail.com>
2024-04-15 22:59:05 -0400

    se añade al archivo view.js los item de dataset.js con nuevas etiquetas html y cambios en los estilos para elementos li en style.css

--- src/view.js
+++ src/view.js
@@ -1,23 +1,52 @@
 export const renderItems = (data) => {
-    const newUl = document.createElement("ul")
+  const newUl = document.createElement("ul");
 
-    data.forEach(element => {
-        console.log(element)
-        const newLi = document.createElement("li")
-        const newImage = document.createElement("img")
-        const cafe = element
-        newImage.setAttribute("src", cafe.imageUrl)
-        newLi.appendChild(newImage)
-        const cafe2 = document.createElement("h5")
-        const cafeZ = element
-        cafe2.textContent = cafeZ.name
-        newLi.appendChild(cafe2)
-        newUl.appendChild(newLi)
-    });
-    document.body.appendChild(newUl)
-    console.log(data)
-    //   Aquí comienza tu código y puedes retornar lo que tu necesites
-    return 'example';// debe retornar solo newUl 
+  data.forEach(element => {
+    //console.log(element)
+    const newLi = document.createElement("li"); //un elemento li por cada elemento en data
+    const newImage = document.createElement("img"); //un elemento img para mostrar la imagen
+    
+    newImage.setAttribute("src", element.imageUrl); //utilizamos la url de la imagen del doc actual
+    newLi.appendChild(newImage);
+
+    const shortDescription = document.createElement("span");
+    shortDescription.textContent = element.shortDescription;
+    
+
+    const cafeName = document.createElement("h5")
+    cafeName.textContent = element.name; //añadimos el nombre al elemento actual
+
+    const cafeDescription = document.createElement("p");
+    cafeDescription.textContent = element.description;
+    //Creamos una lista para los hechos de cada café
+    const factsList = document.createElement('ul');
+    // Ordenamos manualmente los hechos en el orden deseado
+    const orderedFacts = {
+      "Origen": element.facts.origen,
+      "Tiempo de Extracción": element.facts.tiempoDeExtraccion,
+      "Molienda": element.facts.molienda,
+      "Contenido": element.facts.contenido
+    };
+
+    // Recorremos los hechos ordenados y creamos un elemento <li> para cada hecho
+    for (const [factName, factValue] of Object.entries(orderedFacts)) {
+      if (factValue) {
+        const factItem = document.createElement('li');
+        factItem.textContent = `${factName}: ${factValue}`;
+        factsList.appendChild(factItem);
+      }
+    }
+    newLi.appendChild(cafeName);
+    newLi.appendChild(shortDescription);
+    newLi.appendChild(cafeDescription);
+    newUl.appendChild(newLi);
+    newLi.appendChild(factsList);
+    
+  });
+  document.body.appendChild(newUl)
+  //console.log(data)
+  //   Aquí comienza tu código y puedes retornar lo que tu necesites
+  return newUl;// debe retornar solo newUl 
 
 };
 
