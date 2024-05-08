e7c907469d04656d23566084caf27e2e542101f7
guisadev <giseesaavedra537@gmail.com>
2024-05-08 17:29:15 -0400

    ultimos cambios visual

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,26 @@
-// Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
+//Aquí voy a filtrar de la data un facts que quiero facilitar:
+export const filterData = (data, filterBy, value) => {
+  if (!value) {
+    return data;
+  }
 
-export const example = () => {
-  return 'example';
+  // console.log('filtrado por', filterBy);
+  return data.filter((bebida) => bebida.facts[filterBy].toLowerCase() === value.toLowerCase());
 };
 
-export const anotherExample = () => {
-  return [];
+export const sortData = (data, propertyName, sortOrder) => {
+  return data.slice().sort((a, b) => { //se crea una copia superficial para analizar según el valor de sortOrder
+    //const nameA = a[propertyName].toUpperCase();
+    const nameA =
+      typeof a[propertyName] === "string" ? a[propertyName].toUpperCase() : "";
+    const nameB =
+      typeof b[propertyName] === "string" ? b[propertyName].toUpperCase() : "";
+    //const nameB = b[propertyName].toUpperCase();
+    if (sortOrder === "asc") {
+      return nameA.localeCompare(nameB);
+    } else if (sortOrder === "desc") {
+      return nameB.localeCompare(nameA);
+    }
+    return 0;
+  });
 };
--- src/view.js
+++ src/view.js
@@ -1,47 +1,140 @@
-export const renderItems = (data) => {
-  const newUl = document.createElement("ul");
+import { filterData } from "./dataFunctions.js";
 
-  data.forEach(element => {
+// creo el select
+export const createSelect = (data, propertyName) => {
+  // Obtener todos los valores únicos de la propiedad especificada en los datos
+  const propertyValues = data.map((item) => item.facts[propertyName]);
+
+  // Eliminar valores duplicados usando un conjunto (Set)
+  // eslint-disable-next-line no-undef
+  const uniqueValues = new Set(propertyValues);
+  const span = document.createElement("span")
+  const label = document.createElement("label");
+  label.setAttribute("for", propertyName)
+
+  const select = document.createElement("select");
+  select.setAttribute("id", propertyName)
+  select.setAttribute("name", propertyName)
+  select.setAttribute("class", "select-style")
+ 
+  // Agregar una opción adicional al principio del select que no esté seleccionada
+  const defaultOption = document.createElement("option");
+  defaultOption.value = ""; // Valor vacío
+  defaultOption.textContent = `Seleccione ${propertyName} (sin filtro)`; // Texto descriptivo
+  //defaultOption.disabled = true; // Deshabilitar la opción para que no se pueda seleccionar
+  //defaultOption.selected = true; // No seleccionar la opción por defecto
+  select.appendChild(defaultOption);
+
+  // Agregar una clase al select
+  select.classList.add("select-style");
+  // Agregar una opción por cada valor único de la propiedad
+  uniqueValues.forEach((value) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = value;
+    optionElement.textContent = value;
+    select.appendChild(optionElement);
+  });
+  span.append(label, select)
+  return span;
+};
+// Crear el selectSort
+export const createSelectSort = (data, propertyName) => {
+  const select = document.createElement("select");
+
+  // Agregar una opción adicional al principio del select que no esté seleccionada
+  const defaultOption = document.createElement("option");
+  defaultOption.value = ""; // Valor vacío
+  defaultOption.textContent = `Categoría ${propertyName} (sin filtro)`; // Texto descriptivo
+  //defaultOption.disabled = true; // Deshabilitar la opción para que no se pueda seleccionar
+  //defaultOption.selected = true; // No seleccionar la opción por defecto
+  select.appendChild(defaultOption);
+
+  // Agregar opciones para orden ascendente y descendente
+  const ascendingOption = document.createElement("option");
+  ascendingOption.value = "asc";
+  ascendingOption.textContent = "Ordenar ascendente";
+  select.appendChild(ascendingOption);
+
+  const descendingOption = document.createElement("option");
+  descendingOption.value = "desc";
+  descendingOption.textContent = "Ordenar descendente";
+  select.appendChild(descendingOption);
+
+  return select;
+};
+
+export const renderItems = (data, value = null, filterBy = null) => {
+  // Eliminar la lista ul existente del DOM si existe
+  const existingUl = document.querySelector("ul");
+  if (existingUl) {
+    existingUl.parentNode.removeChild(existingUl);
+  }
+
+  const newUl = document.createElement("ul");
+  
+  
+  // Filtrar los datos según el origen especificado si existe
+  const filteredData = value ? filterData(data,filterBy, value) : data;
+
+  filteredData.forEach((element) => {
     //console.log(element)
     const newLi = document.createElement("li"); //un elemento li por cada elemento en data
+    newLi.setAttribute("itemscope", "")
+    newLi.setAttribute("itemType", element.id)
 
     const newImage = document.createElement("img"); //un elemento img para mostrar la imagen
     newImage.setAttribute("src", element.imageUrl); //utilizamos la url de la imagen del doc actual
-    
-    const name = document.createElement("dt")
-    name.textContent = "Nombre:"
-    const cafeName = document.createElement("dd")
+
+    const name = document.createElement("dt");
+    name.textContent = "Nombre:";
+    const cafeName = document.createElement("dd");
     cafeName.textContent = element.name; //añadimos el nombre al elemento actual
 
     const shortDescription = document.createElement("dt");
-    shortDescription.textContent = "Descripción:"    
+    shortDescription.textContent = "Descripción:";
     const cafeDescription = document.createElement("dd");
     cafeDescription.textContent = element.shortDescription;
 
     //Creamos una lista para los hechos de cada café
     const origen = document.createElement("dt");
     origen.textContent = "Café origen:";
-    const cafeOrigen = document.createElement("dd")
-    cafeOrigen.textContent = element.facts.origen
+    const cafeOrigen = document.createElement("dd");
+    cafeOrigen.textContent = element.facts.origen;
 
-    const tiempoDeExtraccion = document.createElement("dt")
+    const tiempoDeExtraccion = document.createElement("dt");
     tiempoDeExtraccion.textContent = "Tiempo de extracción:";
     const cafeTiempoDeExtraccion = document.createElement("dd");
-    cafeTiempoDeExtraccion.textContent = element.facts.tiempoDeExtraccion
+    cafeTiempoDeExtraccion.textContent = element.facts.tiempoDeExtraccion;
 
-    const contenido = document.createElement("dt");
-    contenido.textContent = "Contenido:";
-    const cafeContenido = document.createElement("dd");
-    cafeContenido.textContent = element.facts.contenido;
+    const porcentajeDeContenido = document.createElement("dt");
+    porcentajeDeContenido.textContent = "Porcentaje de contenido:";
+    const cafePorcentajeDeContenido = document.createElement("dd");
+    cafePorcentajeDeContenido.textContent = element.facts.porcentajeDeContenido;
+
+    const molienda = document.createElement("dt");
+    molienda.textContent = "Molienda:";
+    const cafeMolienda = document.createElement("dd");
+    cafeMolienda.textContent = element.facts.molienda;
 
 
-    newLi.append(newImage, name, cafeName, shortDescription, cafeDescription, origen, cafeOrigen, tiempoDeExtraccion, cafeTiempoDeExtraccion, contenido, cafeContenido);
-    newUl.appendChild(newLi)
-    
+    newLi.append(
+      cafeName,
+      newImage,
+      shortDescription,
+      cafeDescription,
+      origen,
+      cafeOrigen,
+      molienda,
+      cafeMolienda,
+      porcentajeDeContenido,
+      cafePorcentajeDeContenido,
+
+    );
+    newUl.appendChild(newLi);
   });
-  //console.log(data)
-  //   Aquí comienza tu código y puedes retornar lo que tu necesites
-  return newUl;// debe retornar solo newUl 
+  // Agregar la nueva lista al body
+  // Agregar el select al contenedor div
 
+  //document.body.appendChild(newUl);
+  return newUl
 };
-
